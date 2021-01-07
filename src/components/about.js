d3fc95bd1b97328e69f1f2624aac728205a292bc
fmau <florence@maurice-web.de>
2021-01-07 14:12:42 +0100

    Add files via upload

---
+++ src/components/about.js
@@ -0,0 +1,36 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import RobotImage from "../components/robotimage"
+import style from "./events.module.css"
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <SEO
+        title="Infos über diese Site"
+        description="Informationen über diese Site"
+        image="/logo.png"
+        pathname="/"
+        // Boolscher Wert, ob article:
+        // article
+      />
+      <section className={style.wrapper}>
+        <h1 className={style.heading}>Über diese Site</h1>
+        <RobotImage
+          src={"/images/bubbles-callout.png"}
+          alt={"Bubbles, der Roboter"}
+        />
+        <RobotImage src={"/images/dolly-callout.png"} alt={"Roboter Dolly"} />
+        <RobotImage src={"/images/eileen-callout.png"} alt={"Roboter Eileen"} />
+        <div>
+          <p>Ein erster Absatz in der neu erstellten Seite</p>
+          <p>Diese Seite basiert auf index.js.</p>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default AboutPage
